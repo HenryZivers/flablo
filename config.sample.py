511c42af7037810df96a38dace3650a014156b4f
root <root@li1198-193.members.linode.com>
2016-09-28 06:42:47 +0000

    update

--- config.sample.py
+++ config.sample.py
@@ -2,6 +2,12 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+SECRET_KEY = ''
+
+# mysql-python / mysqldb
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://<user_name>:<user_pass>@localhost/<db_name>'
+# pymysql
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<user_name>:<user_pass>@localhost/<db_name>'
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
